97fec6350a20702deec46a4c8a06a3686f222d8c
Matt Dawkins <matthew.dawkins@gmail.com>
2017-06-29 19:43:37 +0100

    Use gulp to include Bootstrap and jQuery and compile everything
    Compiled assets ignored from repository

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -12,3 +12,15 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./resources/assets/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('vendor_js', function() {
+    return gulp.src(['./node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js', './node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('vendor_fonts', function() {
+    return gulp.src('./node_modules/bootstrap-sass/assets/fonts/**/*')
+        .pipe(gulp.dest('./public/fonts/'));
+});
+
+gulp.task('compile', ['sass', 'vendor_fonts', 'vendor_js']);
---
+++ src/resources/views/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <h1>This is a test page</h1>
+    <p>Here is some content. <span class="glyphicon glyphicon-ok"></span></p>
+    <script src="{{ asset('js/jquery.min.js') }}" charset="utf-8"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}" charset="utf-8"></script>
+</body>
+</html>
